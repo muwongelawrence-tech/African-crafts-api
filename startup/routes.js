d5a9ddef04d8299bff96ea91dfd1eebd8f4dcdf9
muwonge lawrence <muwongelawrence44@gmail.com>
2021-09-15 15:29:16 +0300

    few changes

--- startup/routes.js
+++ startup/routes.js
@@ -3,12 +3,14 @@ const shoes = require("../routes/shoes");
 const users = require("../routes/users");
 const materials = require("../routes/materials");
 const auth = require("../routes/auth");
+const cors = require('cors');
 const error = require("../middleware/error");
 const express = require('express');
 
 module.exports =  function(app) {
     app.use(express.json());
     app.use(express.static('public'));
+    app.use(cors());
     app.use('/',root);
     app.use('/api/users',users);
     app.use('/api/shoes',shoes);
